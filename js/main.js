7bec72475c1a5904d4685ae79d5a22453c362e14
Aruncho <aarongabriel20@hotmail.com>
2020-06-22 20:51:49 -0400

    commit is2.0

--- js/main.js
+++ js/main.js
@@ -72,6 +72,23 @@ jQuery(document).ready(function ($) {
       $("#mobile-nav, #mobile-nav-toggle").hide();
     }
   
+    // Porfolio - uses the magnific popup jQuery plugin
+  $('.portfolio-popup').magnificPopup({
+    type: 'image',
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function (openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+  });
   
   
   });
